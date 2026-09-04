4a81874828d65fad68d46082fd45d197cfdeefe4
kashorwork-a11y <kashor.work@gmail.com>
2026-09-04 22:09:49 +0300

    Refactor PdfSigner import and render method

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PdfSigner from './src/PdfSigner';
+import PdfSigner from './PdfSigner.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <PdfSigner />
-  </React.StrictMode>
-);
+root.render(React.createElement(PdfSigner));
